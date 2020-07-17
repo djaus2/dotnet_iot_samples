71cd612940593093866917a9dc923282e0f9e6d4
djaus2 <davidjones@sportronics.com.au>
2020-07-17 13:47:42 +1000

    Add Markdown Component

---
+++ GetAnIOTSampleApp/Client/Components/MarkdownModel.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace GetAnIOTSampleApp.Client.Components
+{
+    public partial class MarkdownModel
+    {
+
+        public string _content;
+
+        [Parameter]
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                HtmlContent = ConvertStringToMarkupString(_content);
+            }
+        }
+
+        public MarkupString HtmlContent { get; private set; }
+
+        private MarkupString ConvertStringToMarkupString(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(_content))
+            {
+                // Convert markdown string to HTML
+                var html = Markdig.Markdown.ToHtml(value, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+
+                // Sanitize HTML before rendering
+                var sanitizedHtml = html; // HtmlSanitizer.Sanitize(html);
+
+                // Return sanitized HTML as a MarkupString that Blazor can render
+                return new MarkupString(sanitizedHtml);
+            }
+
+            return new MarkupString();
+        }
+    }
+}
